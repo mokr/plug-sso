08791cebbfcff093c85fc07b2f3ccb8f3b7a05f2
Morten Kristoffersen <morten.kristoffersen@ice.net>
2022-01-04 09:51:57 +0100

    Ensure Datalevin gets absolute path

--- src/clj/plug_sso/db/core.clj
+++ src/clj/plug_sso/db/core.clj
@@ -1,11 +1,15 @@
 (ns plug-sso.db.core
   "Schema and conn for DB"
-  (:require [datalevin.core :as d]
+  (:require [clojure.java.io :as io]
+            [datalevin.core :as d]
             [plug-sso.config :refer [env]]
             [plug-sso.db.schema :refer [schema]]
-            [mount.core :refer [defstate]]))
+            [mount.core :refer [defstate]]
+            [taoensso.timbre :as log]))
 
 
 (defstate conn
-  :start (d/get-conn (:datalevin-db-path env) schema)
+  :start (let [db-path (-> env :datalevin-db-path io/file .getAbsoluteFile str)] ;; Ensure it is an absolute path as Datalevin did not handle ~/ well and created a '~' sub dir inside project
+           (log/info (format "Connecting to Datalevin DB %s" db-path))
+           (d/get-conn db-path schema))
   :stop (d/close conn))
